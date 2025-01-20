25c90bb4fe5e548f0c63472605b94ffc16183c74
AzureAD\RobertOwen <r.owen@accredit-solutions.com>
2025-01-20 08:27:34 +0000

    added unit testing

---
+++ HRDemo.Api.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace HRDemo.Api.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ HRDemo.Web.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace HRDemo.Web.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
